eeb2721624bf15fab8ff79bb1fdd877ab8082a2d
Roger-luo <rogerluo.rl18@gmail.com>
2023-11-21 13:56:25 -0500

    update test for adding same term

--- test/expr/op/syntax.jl
+++ test/expr/op/syntax.jl
@@ -1,5 +1,4 @@
 using Liang.Expression.Prelude
 using Liang.Tree
-ex = Op.Sum(1:5, Op.X[index"i"] * Op.X[index"i" + 1] + Op.Z[index"i"] * Op.Z[index"i" + 1])
-Basis(Op.X, Space.Qubit)^2
-Tree.inline_print(ex |Basis(Op.X, Space.Qubit)^2 + kron(Op.X, Op.X, Op.I))
+ex = Op.Sum(1:5, (Op.X[index"i"]%Qubit) * (Op.X[index"i" + 1]%Qubit) + Op.Z[index"i"] * Op.Z[index"i" + 1])
+(ex + ex)
