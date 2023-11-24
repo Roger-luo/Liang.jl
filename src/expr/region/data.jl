60d5c9b5be3c61203e3838657eef39b1d765768c
Roger-luo <rogerluo.rl18@gmail.com>
2023-11-24 11:58:25 -0500

    add some desc of region

--- src/expr/region/data.jl
+++ src/expr/region/data.jl
@@ -1,5 +1,20 @@
-# maybe we should use the chain complex?
+"""
+Region conceptually is equivalent to
+an iterator that returns `Vector{Int}`.
+This is used for specifying the following:
+
+- reduction of sum expression.
+- a chain complex (e.g sites, bonds, plaquettes).
+
+Formally, the `Region` is defined as a chain complex
+over integer sets. Implementation-wise, we use more
+structural expression to represent the region to improve
+storage efficiency and retain more information about user
+input.
+"""
 @data Region begin
+    UnitRange(UnitRange{Int})
+
     struct ToInf
         start::Int
         step::Int
