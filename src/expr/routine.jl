a28cf2cf817c8f83b2ad8c56bf7be6cde2ba03a9
Roger-luo <rogerluo.rl18@gmail.com>
2023-12-15 22:37:41 -0500

    routine should store the types of args

--- src/expr/routine.jl
+++ src/expr/routine.jl
@@ -2,5 +2,11 @@ struct Routine{E}
     name::Symbol
     # Variable.Slot
     args::Vector{Symbol}
+    # scan expression types
+    # of the corresponding variable
+    # at construction, so we can validate
+    # the input without walk through the
+    # expression.
+    types::Vector{Any}
     body::E
 end
